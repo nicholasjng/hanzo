a7d096336d72248e3cfa8d7bada64d2004c26b7d
Nicholas Junge <nicho.junge@gmail.com>
2026-03-16 13:51:57 +0100

    Add feature machinery, refactor target structs into proper submodule
    This is in preparation of build config via features. A feature is basically
    a named class carrying a set of settings with it, that are activated via a
    feature flag (in the current case, `--feature` on the PEP517 command line).

---
+++ src/hanzo/features.py
@@ -0,0 +1,91 @@
+from packaging.specifiers import Specifier
+
+from hanzo.types import Define
+
+_SABI_MAP: dict[str, str] = {
+    f"3.{minor}": f"0x{(3 << 24) + (minor << 16):08X}" for minor in range(1, 16)
+}
+
+
+class Feature:
+    """A class representing a feature for a specific build target.
+
+    A feature groups different attributes together, such as build flags,
+    defines, and other configuration. Its main use is to toggle specific
+    build parameters.
+    """
+
+    def __init__(self, name: str):
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+
+class CcFeature(Feature):
+    def __init__(self, name: str):
+        super().__init__(name=name)
+
+        self._flags: list[str] = []
+        self._linkflags: list[str] = []
+        self._defines: list[Define] = []
+
+    @property
+    def flags(self) -> list[str]:
+        return self._flags
+
+    @property
+    def linkflags(self) -> list[str]:
+        return self._linkflags
+
+    @property
+    def defines(self) -> list[Define]:
+        return self._defines
+
+
+class StableABI(CcFeature):
+    """A feature adding the stable ABI macro to a target and its dependents."""
+
+    def __init__(self, abi_spec: str):
+        super().__init__(name="stable-abi")
+
+        abi_specifier = Specifier(abi_spec)
+        abi_version = abi_specifier.version
+        if abi_specifier.operator != ">=" or not abi_version.startswith("3"):
+            raise ValueError(f"stable ABI specifier should be given as '>=3.x', got {abi_spec!r}")
+
+        try:
+            abi_hex = _SABI_MAP[abi_version]
+        except KeyError:
+            raise ValueError(f"unsupported stable ABI version {abi_version!r}")
+
+        self._defines.append(Define(name="Py_LIMITED_API", value=abi_hex))
+
+
+_FEATURE_MAPPING: dict[str, type[Feature]] = {
+    "stable-abi": StableABI,
+}
+
+
+def get_feature(name: str) -> type[Feature]:
+    try:
+        return _FEATURE_MAPPING[name]
+    except KeyError:
+        raise ValueError(f"unknown feature {name!r}") from None
+
+
+def register_feature(name: str, feature: type[Feature], force: bool = False) -> None:
+    if name in _FEATURE_MAPPING and not force:
+        raise ValueError(f"feature {name!r} already registered, force registration with force=True")
+
+    _FEATURE_MAPPING[name] = feature
+
+
+__all__ = [
+    "Feature",
+    "CcFeature",
+    "StableABI",
+    "get_feature",
+    "register_feature",
+]
---
+++ src/hanzo/types.py
@@ -0,0 +1,96 @@
+import os
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Required, Self, TypedDict
+
+
+class GlobDict(TypedDict, total=False):
+    include: Required[list[str]]
+    exclude: list[str]
+    allow_empty: bool
+
+
+@dataclass(frozen=True)
+class FileGlob:
+    include: list[str]
+    exclude: list[str] = field(default_factory=list)
+    allow_empty: bool = False
+
+    def resolve(self) -> list[str]:
+        results: list[str] = []
+        for p in self.include:
+            star = p.find("*")
+            if star == -1:
+                if p not in self.exclude:
+                    results.append(p)
+            else:
+                path, pattern = Path(p[:star]), p[star:]
+                # paths are relative to cwd, so use with chdir().
+                for res in path.glob(pattern):
+                    res = str(res)
+                    if res not in self.exclude:
+                        results.append(res)
+        if not results and not self.allow_empty:
+            raise ValueError(f"glob pattern {self.include} did not yield any results")
+
+        return results
+
+
+class IncludeDict(TypedDict, total=False):
+    path: Required[str]
+    system: bool
+    local: bool
+
+
+@dataclass(frozen=True)
+class Include:
+    path: str | os.PathLike[str]
+    system: bool = False
+    local: bool = False
+
+    def __str__(self) -> str:
+        prefix = "-isystem" if self.system else "-I"
+        return prefix + Path(self.path).as_posix()
+
+
+class DefineDict(TypedDict, total=False):
+    name: Required[str]
+    value: str | int | bool | None
+    local: bool
+
+
+@dataclass(frozen=True)
+class Define:
+    name: str
+    value: str | int | bool | None = None
+    local: bool = False
+
+    @classmethod
+    def from_literal(cls, lit: str) -> Self:
+        if lit.startswith("-D"):
+            lit = lit[2:]
+
+        try:
+            name, val = lit.split("=", 1)
+        except ValueError:
+            # true-ish define like -DNDEBUG
+            name, val = lit, True
+        # if you want a local define, use a TOML struct.
+        return cls(name, val)
+
+    def __str__(self) -> str:
+        sdefine = "-D" + self.name
+        if isinstance(self.value, bool | None):
+            return sdefine
+        else:
+            return sdefine + "=" + str(self.value)
+
+
+__all__ = [
+    "GlobDict",
+    "IncludeDict",
+    "DefineDict",
+    "FileGlob",
+    "Include",
+    "Define",
+]
