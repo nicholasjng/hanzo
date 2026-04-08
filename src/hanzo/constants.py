1225dcc0e017635778d2a465b8de1c2d3e25661e
Nicholas Junge <nicho.junge@gmail.com>
2026-04-08 12:21:18 +0200

    Add platform-specific flag parsing, unit tests
    Offers both the per-platform config case (more likely) and the cross-platform case (unlikely).
    Adds unit tests that cover both cases.

--- src/hanzo/constants.py
+++ src/hanzo/constants.py
@@ -8,4 +8,4 @@ DEFAULT_PY_TOOLCHAIN_NAME: str = "current"
 DEFAULT_BUILD_DIR = "build"
 
 # default source file extensions
-DEFAULT_SOURCE_EXTENSIONS: frozenset[str] = frozenset({".py", ".cpp", ".c", ".h", ".hpp", ".pyi"})
+DEFAULT_SOURCE_EXTENSIONS: frozenset[str] = frozenset({".py", ".pyi"})
